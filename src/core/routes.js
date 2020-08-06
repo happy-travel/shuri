f48a83cce3beac3eb76b627d82e5f77ab1e23be2
alexander@plekhanov.net <hQG1gLwk>
2020-08-06 21:24:18 +0300

    Contract page implementation.

--- src/core/routes.js
+++ src/core/routes.js
@@ -3,10 +3,19 @@ import { Switch } from 'react-router-dom';
 import Route from 'matsumoto/src/core/misc/route';
 import errorPage from 'matsumoto/src/pages/common/error';
 import mainPage from 'pages/main';
+import contractPage from 'pages/contract-page';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={mainPage} />
+        <Route
+            exact
+            path={[
+                "/contract/:id",
+                "/contract"
+            ]}
+            component={contractPage}
+        />
         <Route component={errorPage} />
     </Switch>
 );
