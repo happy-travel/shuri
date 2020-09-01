38ed27f43c6e0fe67d0236b794d1a7a70106c763
Vsevolod Petrushchenko <69117259+kuskido@users.noreply.github.com>
2020-09-02 00:48:33 +0300

    HIR-70: Implement time picker (#16)

---
+++ src/components/form/field-time.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { observer } from 'mobx-react';
+import View from 'matsumoto/src/stores/view-store';
+import FieldText from 'matsumoto/src/components/form/field-text';
+
+const TIME_REGEXP = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+
+@observer
+class SelectDropdown extends React.Component {
+    setValue = (item) => {
+        const { formik } = this.props;
+        const { value } = item;
+        if (!formik) {
+            return;
+        }
+
+        formik.setFieldValue(this.props.connected, value);
+        this.props.setValue(value);
+        View.setOpenDropdown(null);
+    }
+
+    render() {
+        const { options } = this.props;
+
+        return (
+            <div className="dropdown select">
+                {options?.map((item) => (
+                    <div key={item.value} className="item line" onClick={ () => this.setValue(item) }>
+                        {item.text}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+@observer
+class FieldTime extends React.Component {
+    lastCorrectValue;
+
+    setLastCorrectValue = (value) => {
+        if (TIME_REGEXP.test(value)) {
+            this.lastCorrectValue = value;
+        }
+    }
+
+    onBlur = () => {
+        this.props.formik.setFieldValue(this.props.id, this.lastCorrectValue || '');
+    }
+
+    onChange = (event) => {
+        const previousValue = this.props.formik.values[this.props.id];
+        const { value } = event.target;
+        if (previousValue?.length === 1) {
+            event.target.value = value.replace(/^\d\d$/, '$&:');
+        }
+        this.setLastCorrectValue(previousValue);
+        this.setLastCorrectValue(value);
+        View.setOpenDropdown(null);
+    }
+
+    onDropdownClick = (value) => {
+        this.setLastCorrectValue(value);
+    }
+
+    render() {
+        const { options } = this.props;
+        return (
+            <FieldText
+                {...this.props}
+                maxLength={5}
+                setValue={options ? this.onDropdownClick : undefined}
+                onBlur={this.onBlur}
+                onChange={this.onChange}
+                Dropdown={options ? SelectDropdown : undefined}
+            />
+        );
+    }
+}
+
+SelectDropdown.propTypes = {
+    options: propTypes.array,
+    formik: propTypes.object,
+    connected: propTypes.string,
+    setValue: propTypes.func
+};
+
+FieldTime.propTypes = {
+    options: propTypes.array,
+    formik: propTypes.object,
+    id: propTypes.string
+};
+
+export default FieldTime;
--- src/pages/accommodation/accommodation-page.js
+++ src/pages/accommodation/accommodation-page.js
@@ -13,12 +13,27 @@ import Breadcrumbs from 'matsumoto/src/components/breadcrumbs';
 import UI from 'stores/shuri-ui-store';
 import LocationsStore from 'stores/shuri-locations-store';
 import DialogModal from 'parts/dialog-modal';
+import FieldTime from 'components/form/field-time';
 import {
     createAccommodation,
     getAccommodation,
     removeAccommodation,
     updateAccommodation
 } from 'providers/api';
+
+const CHECKOUT_TIME_OPTIONS = [
+    { value: '10:00', text: '10:00' },
+    { value: '11:00', text: '11:00' },
+    { value: '12:00', text: '12:00' },
+    { value: '13:00', text: '13:00' },
+    { value: '14:00', text: '14:00' }
+]
+
+const CHECKIN_TIME_OPTIONS = [
+    { value: '14:00', text: '14:00' },
+    { value: '15:00', text: '15:00' },
+    { value: '16:00', text: '16:00' }
+]
 
 @observer
 class AccommodationPage extends React.Component {
@@ -232,20 +247,20 @@ class AccommodationPage extends React.Component {
                     />
                 </div>
                 <div className="row">
-                    <FieldText
+                    <FieldTime
                         formik={formik}
-                        clearable
                         id="checkInTime"
                         label={'Check In Time'}
                         placeholder={'16:00'}
+                        options={CHECKIN_TIME_OPTIONS}
                         required
                     />
-                    <FieldText
+                    <FieldTime
                         formik={formik}
-                        clearable
                         id="checkOutTime"
                         label={'Check Out Time'}
                         placeholder={'11:00'}
+                        options={CHECKOUT_TIME_OPTIONS}
                         required
                     />
                 </div>
