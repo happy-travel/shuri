5dff84b5ad8b435d211c9e70ac4908f1b6a5d886
kuskido <vsevolod.petrushchenko@happytravel.com>
2020-10-05 01:09:19 +0300

    Separate availability pages

---
+++ src/pages/availability/room-availability-calendar.js
@@ -0,0 +1,168 @@
+import React from 'react';
+import { action, computed, observable } from 'mobx';
+import { observer } from 'mobx-react';
+import { withTranslation } from 'react-i18next';
+import { Redirect } from 'react-router-dom';
+import propTypes from 'prop-types';
+import { Loader } from 'matsumoto/src/simple';
+import Breadcrumbs from 'matsumoto/src/components/breadcrumbs';
+import { CachedForm } from 'matsumoto/src/components/form';
+import CalendarForm from 'components/calendar';
+import {
+    getAvailabilityRestrictions,
+    getContract,
+    getContractAccommodations,
+    updateAvailabilityRestrictions
+} from 'providers/api';
+import UI from 'stores/shuri-ui-store';
+import { convertRestrictionsToForm, convertFormToRestrictions } from 'pages/calendar/utils/converter';
+
+@observer
+class RoomAvailabilityCalendar extends React.Component {
+    @observable redirectUrl;
+    @observable roomsList;
+    @observable contract;
+    @observable availabilityRestrictions;
+    @observable roomId = this.props.match.params.roomId;
+    contractId = this.props.match.params.contractId;
+    possibleCalendarValues = {
+        StopSale: this.props.t('Stop sale'),
+        Allotment: this.props.t('Allotment'),
+        FramedFreeSale: this.props.t('Framed free sale'),
+        FreeSale: this.props.t('Free sale')
+    };
+
+    @computed
+    get initialCalendarValues() {
+        if (!this.availabilityRestrictions || !this.contract) {
+            return undefined;
+        }
+        return convertRestrictionsToForm(this.availabilityRestrictions, this.contract);
+    }
+
+    constructor(props) {
+        super(props);
+        const requestParams = { urlParams: { id: this.contractId } };
+        Promise.all([
+            getContract(requestParams),
+            getContractAccommodations(requestParams),
+            getAvailabilityRestrictions({
+                urlParams: {
+                    contractId: this.contractId,
+                    roomId: this.roomId
+                }
+            })
+        ]).then(this.getDataSuccess);
+    }
+
+    @action
+    getDataSuccess = ([contract, accommodationsList, restrictions]) => {
+        this.roomsList = new Map();
+        this.contract = contract;
+        accommodationsList.forEach((accommodation) => {
+            accommodation.roomIds?.forEach((roomId) => {
+                this.roomsList.set(String(roomId), `#${roomId}: ${accommodation.name[UI.editorLanguage]}`);
+            });
+        });
+        this.availabilityRestrictions = restrictions;
+    }
+
+    onSubmit = (values) => {
+        updateAvailabilityRestrictions({
+            urlParams: {
+                id: this.contractId
+            },
+            body: convertFormToRestrictions(values, this.contract, Number(this.roomId))
+        }).then(this.updateAvailabilityRestrictionsSuccess);
+    }
+
+    @action
+    updateAvailabilityRestrictionsSuccess = () => {
+        this.redirectUrl = `/contract/${this.contractId}/availability/rooms`;
+    }
+
+    renderBreadcrumbs = () => {
+        const { t } = this.props;
+        return (
+            <Breadcrumbs
+                backLink={`/contract/${this.contractId}/availability/rooms`}
+                items={[
+                    {
+                        text: t('Contracts'),
+                        link: '/contracts'
+                    },
+                    {
+                        text: this.contract.name || `Contract #${this.contractId}`,
+                        link: `/contract/${this.contractId}`
+                    },
+                    {
+                        text: t('Rooms'),
+                        link: `/contract/${this.contractId}/availability/rooms`
+                    },
+                    {
+                        text: t('Availability Calendar') + ` - ${this.roomsList.get(this.roomId)}`
+                    }
+                ]}
+            />
+        );
+    }
+
+    renderCalendar = () => {
+        if (this.initialCalendarValues === undefined) {
+            return <Loader />;
+        }
+
+        return (
+            <div className="calendar-table availability-calendar">
+                <CachedForm
+                    initialValues={this.initialCalendarValues}
+                    onSubmit={this.onSubmit}
+                    render={(formik) => {
+                        return (
+                            <div className="form">
+                                <CalendarForm
+                                    formik={formik}
+                                    possibleValues={this.possibleCalendarValues}
+                                    startDate={this.contract.validFrom}
+                                    endDate={this.contract.validTo}
+                                />
+                            </div>
+                        );
+                    }}
+                    enableReinitialize
+                />
+            </div>
+        );
+    }
+
+    render() {
+        if (this.contract === undefined) {
+            return <Loader />;
+        }
+        if (this.redirectUrl) {
+            return <Redirect push to={this.redirectUrl} />;
+        }
+        return (
+            <>
+                <div className="settings block">
+                    <section>
+                        {this.renderBreadcrumbs()}
+                        <h2>
+                            <span className="brand">
+                                {this.props.t('Availability Calendar') + ` - ${this.roomsList.get(this.roomId)}`}
+                            </span>
+                        </h2>
+                        {this.renderCalendar()}
+                    </section>
+                </div>
+            </>
+        );
+    }
+}
+
+RoomAvailabilityCalendar.propTypes = {
+    t: propTypes.func,
+    match: propTypes.object
+};
+
+export default withTranslation()(RoomAvailabilityCalendar);
