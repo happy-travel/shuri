adb1a07b4449b73aab765db8dcc085b8cf7efa89
kuskido <vsevolod.petrushchenko@happytravel.com>
2020-12-02 10:43:19 +0300

    Calendar new design

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -21,6 +21,13 @@ class CalendarForm extends React.Component {
     @observable selected = [];
     @observable everChanged = false;
 
+    componentDidMount() {
+        const { startDate, formik } = this.props;
+        let date = new Date(startDate);
+        date.setDate(1);
+        formik.setFieldValue('month', Number(date));
+    }
+
     clearSelected = () => {
         this.selected = [];
     }
@@ -119,9 +126,6 @@ class CalendarForm extends React.Component {
         }
         return (
             <div className="month">
-                <h2>
-                    {MONTH_NAMES[date.getMonth()]} {date.getFullYear()}
-                </h2>
                 <table>
                     <tr>
                         {DAY_OF_WEEK_NAMES
@@ -136,13 +140,13 @@ class CalendarForm extends React.Component {
         );
     }
 
-    renderCalendar = (startDate, endDate) => {
+    renderCalendar = (startDate) => {
         const months = [];
         let date = new Date(startDate);
         date.setDate(1);
         for (
             date;
-            date < new Date(endDate);
+            new Date(startDate).getUTCMonth() === date.getUTCMonth();
             date.setMonth(date.getMonth()+1)
         ) {
             months.push(this.renderMonth(date));
@@ -154,40 +158,80 @@ class CalendarForm extends React.Component {
         )
     }
 
+    renderMonthSelector = () => {
+        const { t, formik } = this.props;
+        const { startDate, endDate } = this.props;
+        const options = [];
+
+        let date = new Date(startDate);
+        date.setDate(1);
+        for (
+            date;
+            date < new Date(endDate);
+            date.setMonth(date.getMonth()+1)
+        ) {
+            options.push({
+                value: Number(date),
+                text: MONTH_NAMES[date.getMonth()] + ' ' + date.getFullYear()
+            });
+        }
+
+        return (
+            <FieldSelect
+                formik={formik}
+                label={t('Month')}
+                id="month"
+                options={options}
+            />
+        )
+    }
+
     render() {
         const { t, formik, possibleValues } = this.props;
-        const { startDate, endDate } = this.props;
 
         return (
             <div className="calendar">
-                { Boolean(this.selected?.length) &&
-                    <div className="selector-wrapper">
-                        <button type="button" className="button-clear" onClick={this.clearSelected}>
-                            {t('Clear selection')}
-                        </button>
-                        <FieldSelect
-                            formik={formik}
-                            label={t('Choose value for all selected days')}
-                            id="common-value"
-                            setValue={(value) => this.setValues(value, formik)}
-                            options={Object.keys(possibleValues).map((key) => ({
-                                value: key,
-                                text: possibleValues[key]
-                            }))}
-                        />
+                <div className="top-controls">
+                    <div>
+                        {this.renderMonthSelector()}
                     </div>
-                }
-                {this.renderCalendar(startDate, endDate)}
-                <div className={__class(this.everChanged, 'submit-wrapper', 'hide')}>
-                    <div className="field">
-                        <div className="inner">
-                            <button
-                                type="submit"
-                                className="button"
-                            >
-                                {t('Save changes')}
+                    {Boolean(this.selected?.length) &&
+                        <div className="clear-wrapper">
+                            <button type="button" className="button-clear" onClick={this.clearSelected}>
+                                {t('Clear selection')}
                             </button>
                         </div>
+                    }
+                </div>
+                <div className="main-section">
+                    {this.renderCalendar(formik.values.month)}
+                    <div className="right-controls">
+                        {Boolean(this.selected?.length) &&
+                            <FieldSelect
+                                formik={formik}
+                                label={t('Choose value for all selected days')}
+                                id="common-value"
+                                setValue={(value) => this.setValues(value, formik)}
+                                options={Object.keys(possibleValues).map((key) => ({
+                                    value: key,
+                                    text: possibleValues[key]
+                                }))}
+                            />
+                        }
+                        {this.everChanged &&
+                            <div className="">
+                                <div className="field">
+                                    <div className="inner">
+                                        <button
+                                            type="submit"
+                                            className="button"
+                                        >
+                                            {t('Save changes')}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
